b85cedf1b0f7528e5bc852932a27a91b63c1a29c
shaik-adnan1 <yousufwrk0@gmail.com>
2023-10-06 16:55:01 +0530

    -> created a stream, parse and connected them with a pipeline -> read, parse a csv file

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const { parse } = require('csv-parse');
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+    return planet["koi_disposition"] === "CONFIRMED" 
+    && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(parse({
+    comment: '#',
+    columns: true
+  }))
+  .on("data", data => {
+    if(isHabitable(data)) {
+        habitablePlanets.push(data);
+    }
+  })
+  .on("error", err => {
+    console.log(err);
+  })
+  .on("end", () => {
+    habitablePlanets.map((curPlanetData,i) => {
+        console.log(`${i + 1}: Planet Name: ${curPlanetData.kepler_name}`);
+    });
+    console.log("Done");
+  });
+// parse();
+
